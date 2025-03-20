05dc513e4a44b5f7f080dc61099745bf07e0e64f
j23110444 <164988685+j23110444@users.noreply.github.com>
2025-03-20 11:06:13 +0800

    Update web.py

--- web.py
+++ web.py
@@ -12,7 +12,6 @@ def index():
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=使用者>傳送使用者暱稱</a><br>"
     homepage += "<a href=/account>網頁表單傳值</a><br>"
-    homepage += "<a href=rwd>奕寬簡介網頁(rwd版本)</a><br>"
     homepage += "<a href=http://www1.pu.edu.tw/~a1132001/>奕寬簡介網頁</a><br>"
 
 
@@ -41,11 +40,6 @@ def account():
         return result
     else:
         return render_template("account.html")
-
-@app.route("/rwd")
-def rwd():
-    return render_template("rwd.html")
-
 
 if __name__ == "__main__":
     app.run()
